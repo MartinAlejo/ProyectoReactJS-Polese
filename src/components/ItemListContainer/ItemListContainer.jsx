50cbf85d2a1c788896d9eb4d4dd326d3bf4cd2d9
Martin Polese <mpolese@fi.uba.ar>
2023-01-28 03:52:23 -0300

    Le doy un poco mas de estilizado al componente ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 
 export const ItemListContainer = ({greeting}) => {
   return (
-    <Typography variant="h2" sx={{m: 2}}>{greeting}</Typography>
+    <Typography variant="h2" sx={{m: 4, p: 1, border: 3, borderRadius: 10, display: "flex", justifyContent: "center"}}>{greeting}</Typography>
   )
 }
