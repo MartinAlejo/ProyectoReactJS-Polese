8f4a935644481b1229501851a828256b9401245f
Martin Polese <mpolese@fi.uba.ar>
2023-01-31 18:30:40 -0300

    Realizo la actividad número 1, cuya consigna se encuentra en las diapositivas de la clase 5

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { ItemCount } from "./components/ItemCount/ItemCount";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import {Navbar} from "./components/Navbar/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <ItemListContainer greeting="Hola Mundo!"/>
+      <ItemCount stockInicial={10} />
     </div>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,40 @@
+import { Button } from "@mui/material"
+import { useState } from "react"
+
+export const ItemCount = ({stockInicial}) => {
+
+  const [contador, setContador] = useState(0)
+  const [stock, setStock] = useState(stockInicial)
+
+  const restar = () => {
+    if (contador > 0) {
+      setContador( contador - 1 )
+      setStock(stock + 1)
+    }
+  }
+
+  const sumar = () => {
+    if (stock > 0) {
+      setContador( contador + 1 )
+      setStock(stock - 1)
+    }
+  }
+
+  const agregarAlCarrito = () => {
+    console.log(`Se agregaron ${contador} unidades de este producto al carrito`)
+    setContador(0)
+  }
+
+  console.log(stock)
+
+  return (
+    <>
+      <div style={ {display: "flex", alignItems: "center"} }>
+        <Button variant="outlined" onClick={restar} sx={{ml:5, mr: 1}} disabled={contador === 0}>-</Button>
+        <h2>{contador}</h2>
+        <Button variant="outlined" onClick={sumar} sx={{ml: 1, mr: 5}} disabled={stock <= 0}>+</Button>
+        <Button variant="outlined" onClick={agregarAlCarrito}>Agregar al carrito</Button>
+      </div>
+    </>
+  )
+}
