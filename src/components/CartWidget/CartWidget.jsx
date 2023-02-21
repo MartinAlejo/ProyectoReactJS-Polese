945a0013c32375d444a2bccb8f75b7b62d91a63a
Martin Polese <mpolese@fi.uba.ar>
2023-02-21 18:22:54 -0300

    Ahora el CartWidget muestra la cantidad de productos agregados

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,14 @@
 import { Badge } from '@mui/material'
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
 
 export const CartWidget = () => {
+
+  const { getTotalItems } = useContext(CartContext)
+
   return (
-    <Badge badgeContent={2} color="error">
+    <Badge badgeContent={getTotalItems()} color="error">
       <ShoppingCartRoundedIcon />
     </Badge>
   )
