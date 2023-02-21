befea8070d8206891c9ba89ec249ffbbfedf6da7
Martin Polese <mpolese@fi.uba.ar>
2023-02-21 18:12:10 -0300

    Ahora en el carrito se listan los productos que fueron agregados

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,22 @@
+import { useContext } from "react"
+import { CartContext } from "../../context/CartContext"
+
 export const Cart = () => {
+
+  const { cart } = useContext( CartContext )
+
+  console.log(cart)
+
   return (
-    <div style={{alignSelf: "center"}}><h1> <b> CARRITO </b> </h1> </div>
+
+    <div style={{alignSelf: "center"}}><h1> <b> CARRITO </b> </h1>
+      {cart.map( (prod) => { 
+        return <div key={prod.id} style={{border: "2px solid black", padding: "10px", marginBottom: "15px"}}>
+          <h3> Nombre: {prod.title} </h3>
+          <h3> Precio: {prod.precio} </h3>
+          <h3> Cantidad: {prod.quantity} </h3>
+        </div>
+      })}
+    </div>
   )
 }
